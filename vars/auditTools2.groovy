35c92f629d88652954458947a90bd6c7c841bb97
ghshreyan <shreyan@worldreader.org>
2024-06-20 16:48:33 +0530

    created auditTools2.grrovy

---
+++ vars/auditTools2.groovy
@@ -0,0 +1,14 @@
+// By default the fucntion name within the groovy file should be call(). If any other function name is specified, the pipeline will fail
+
+def call(Map config) {
+
+    // By default all code within a shared library must be executed within the node block
+    node {
+        echo $(config.message)
+        sh '''
+        git version
+        java -version
+        mvn -version
+        '''
+    }
+}
